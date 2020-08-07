5492bcd9158f7050f11038d74b1a26f569dc52e5
learner25 <smslam2999@gmail.com>
2020-08-07 10:08:10 +0600

    yarn here

---
+++ build/precache-manifest.3bedf602364ee10ff24fc4a4ac5d9588.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e187b3113bc315184e6",
+    "url": "/static/js/main.fd5a0760.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aeecfdc8ffb491fb33d5",
+    "url": "/static/js/2.91a60595.chunk.js"
+  },
+  {
+    "revision": "020413ed81fd58865c44271d84e047db",
+    "url": "/index.html"
+  }
+];
