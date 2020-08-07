97fdbe392025c0a59adb1411f04c44afab13714d
learner25 <smslam2999@gmail.com>
2020-08-07 12:05:08 +0600

    minification

---
+++ build/precache-manifest.8ed9f7d374cf469985547149c2a9f0e7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "43a5d463ef9facf0055e",
+    "url": "/static/js/main.e36965c7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9f1594f02d1edfb5eb28",
+    "url": "/static/js/2.ecf5aeca.chunk.js"
+  },
+  {
+    "revision": "7efb844cb1e8d11cffb50ddabebc3ee2",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3bedf602364ee10ff24fc4a4ac5d9588.js"
+  "/precache-manifest.8ed9f7d374cf469985547149c2a9f0e7.js"
 );
 
 workbox.clientsClaim();
